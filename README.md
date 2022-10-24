bd24af1715f0b81868273d4a1177b26daabd38fa
Roberto_Tello <Roberto.Tello@itksquare.edu.mx>
2022-10-23 19:27:09 -0500

    draft

---
+++ README.md
@@ -0,0 +1,4 @@
+# ui_practice
+
+### Practice 1
+![Practice 1](./screenshots/practice_1.png)
---
+++ lib/screens/practice_1.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Practice1 extends StatelessWidget {
+  const Practice1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Text("Hello Flutter!",
+        style: TextStyle(color: Colors.white, fontSize: 20),),
+      ),
+    );
+  }
+}
