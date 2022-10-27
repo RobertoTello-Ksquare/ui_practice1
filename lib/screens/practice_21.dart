f3a1d77b1230f14e1f09ba0083b8a0cbb5edf593
Roberto_Tello <Roberto.Tello@itksquare.edu.mx>
2022-10-27 15:01:36 -0500

    draft

--- lib/screens/practice_21.dart
+++ lib/screens/practice_21.dart
@@ -5,77 +5,118 @@ import 'package:flutter/src/widgets/framework.dart';
 class Practice21 extends StatelessWidget {
    Practice21({super.key});
 
-final List names = [
-  {
-  "item" : "Ipod for sale",
-  "price": 12000,
-  "image": "images/headph.png"
-  },
-  {
-  "item" : "Ipod for sale",
-  "price": 12000,
-  "image": "images/headph.png"
-  },
-  {
-  "item" : "Ipod for sale",
-  "price": 12000,
-  "image": "images/headph.png"
-  },
-  {
-  "item" : "Ipod for sale",
-  "price": 12000,
-  "image": "images/headph.png"
-  },
-  {
-  "item" : "Ipod for sale",
-  "price": 12000,
-  "image": "images/headph.png"
-  },
-  {
-  "item" : "Ipod for sale",
-  "price": 12000,
-  "image": "images/headph.png"
-  },
-  {
-  "item" : "Ipod for sale",
-  "price": 12000,
-  "image": "images/headph.png"
-  },
-  {
-  "item" : "Ipod for sale",
-  "price": 12000,
-  "image": "images/headph.png"
-  },
-  {
-  "item" : "Ipod for sale",
-  "price": 12000,
-  "image": "images/headph.png"
-  }
-/*    "Ipod for sale,",
-    "Ipad for sale ",
-    "IMac for sale",
-    "Samsung Galaxy s22 ultra for sale",
-    "Samsung Galaxy s22 for sale",
-    "Samsung Galaxy s22 plus for sale",
-    "Yeti Mic for sale",
-    "Moto G4 for sale",*/
-   
+final List list = ["item","time","price"];
+  List items =[
+"Ipods for sale",
+"IMac for sale",
+"Samsung Galaxy",
+"IMac Pro for sale",
+"ipad Pro"
+
   ];
+  List time =[
+"5mins ago",
+"5mins ago",
+"5mins ago",
+"5mins ago",
+"5mins ago",
+
+ ];
+  List price =[
+"\$500",
+"\$500",
+"\$500",
+"\$500",
+"\$500",
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
     appBar: AppBar(backgroundColor: Color.fromARGB(255,24,119,242),),
 
-      body: ListView.builder(itemCount: names.length,
-           itemBuilder: ((context, index) {
-             return Container(
-              
-               margin: EdgeInsets.all(8),
-               padding: EdgeInsets.all(12),
-               height: 60,
-               color: Colors.green,
-               width: 50,
-             )
-             
-             ;}),),);
-  }}
+      body: SingleChildScrollView(
+        child:  Column(
+          
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [            
+            SizedBox(
+              height: 50,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                    itemCount: list.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        width: 100,
+                        height: 45,
+                        margin: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: const Color(0xFFf4d3d2)),
+                        child: Center(
+                          child: Text(
+                            list[index],
+                            style: const TextStyle(fontSize: 22),
+                           ),
+                        ),
+                      );
+                    }),
+              ),
+                   Flexible(
+                  child: ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: items.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                            height: 100,
+                            color: const Color(0xFFf6f6f6),
+                            alignment: Alignment.center,
+                            margin: const EdgeInsets.all(5),
+                            padding: const EdgeInsets.all(5),
+                            child: Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Row(
+                                children: [
+                                  Image.asset("images/headph.png",
+                                      fit: BoxFit.cover),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          items[index],
+                                          style: const TextStyle(
+                                              fontSize: 22,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          time[index],
+                                          style: const TextStyle(fontSize: 10),
+                                        ),
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(top: 5),
+                                          child: Text(
+                                            price[index],
+                                            style: const TextStyle(
+                                                fontSize: 18,
+                                                color: Color(0xFFff813a)),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ));
+                      }))
+            ],
+          ),
+        ));
+  }
+}
--- lib/screens/practice_23.dart
+++ lib/screens/practice_23.dart
@@ -3,48 +3,84 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class Practice23 extends StatelessWidget {
-  Practice23({super.key});
+  const Practice23({super.key});
 
- final List names = [
-  {
-  "item" : "Ipod for sale",
-  "price": 12000,
-  "image": "images/headph.png"
-  }
-/*    "Ipod for sale,",
-    "Ipad for sale ",
-    "IMac for sale",
-    "Samsung Galaxy s22 ultra for sale",
-    "Samsung Galaxy s22 for sale",
-    "Samsung Galaxy s22 plus for sale",
-    "Yeti Mic for sale",
-    "Moto G4 for sale",*/
-   
-  ];
-  @override
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(backgroundColor: Color.fromARGB(255,24,119,242),),
-
-      body: ListView.builder(itemCount: names.length,
-           itemBuilder: ((context, index) {
-             return Container(
-              
-               margin: EdgeInsets.all(8),
-               padding: EdgeInsets.all(12),
-               height: 60,
-               color: Colors.green,
-               width: 50,
-               child: Row(children: [
-
-                Container(child: Column(children: [
-                        
-                        Image.asset("images/headph.png")
-                ],),),
-
-                Container(child: Column(children: [
-
-                ],),),
-                ],
-                ),);}),),);}}
-               
+      appBar: AppBar(
+        backgroundColor: Color(0xFF1877f2),
+      ),
+      backgroundColor: Colors.white,
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
+          childAspectRatio: 1.0,
+        ),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Container(
+            margin: const EdgeInsets.all(7),
+            child: Stack(children: [
+              Image.asset(
+                'images/headph.png',
+                height: double.infinity,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              const Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    radius: 15,
+                    backgroundColor: Colors.white,
+                    child: Center(
+                      child: Icon(
+                        Icons.favorite_border,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Container(
+                  height: 50,
+                  color: Color.fromARGB(255, 6, 29, 48).withOpacity(0.5),
+                  child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(left: 5),
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Airpods for sale ",
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.white),
+                                ),
+                                Container(
+                                    margin: const EdgeInsets.only(top: 3),
+                                    child: const Text(
+                                      "\$500",
+                                      style: TextStyle(
+                                          fontSize: 14, color: Colors.white),
+                                    )),
+                              ]),
+                        ),
+                      ]),
+                ),
+              )
+            ]),
+          );
+        },
+      ),
+    );
+  }
+}
